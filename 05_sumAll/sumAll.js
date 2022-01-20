6ea072df29e529a9c11281d2f68543d8c4c2646e
Luka Colić <lcenzo@protonmail.ch>
2022-01-20 14:12:48 +0100

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0) return "ERROR";
+    if(typeof start != 'number' || typeof end != 'number') return "ERROR";
 
+    if(start > end) {
+        let c = start;
+        start = end;
+        end = c;
+    }
+
+    let res = (start + end) / 2 * (end - start + 1);
+
+    return res;
 };
 
 // Do not edit below this line
