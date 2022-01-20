8e10cc48c46e525d19d4b611e66e6eb4aef88fe4
Luka Colić <lcenzo@protonmail.ch>
2022-01-20 14:04:14 +0100

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    for(let i = 1; i < arguments.length; i++) {
+        arr = arr.filter(num => num !== arguments[i]);
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
