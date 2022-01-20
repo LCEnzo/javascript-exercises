5b28f068adb70b48e21225a031e155f9f8981902
Luka Colić <lcenzo@protonmail.ch>
2022-01-20 13:47:18 +0100

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfRepetitions) {
+    let ret = '';
 
+    if(numberOfRepetitions < 0) return "ERROR";
+
+    for(let i = 0; i < numberOfRepetitions; i++) {
+        ret = ret + string;
+    }
+
+    return ret;
 };
 
 // Do not edit below this line
