752eeb83dd3e25bf52f59200d3bf1214d7ed833b
Luka Colić <lcenzo@protonmail.ch>
2022-01-25 09:53:53 +0100

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.trim().toLowerCase().split('').filter(c => c.match(/[a-z]/));
 
+    const len = string.length;
+    const halfLen = len / 2;
+
+    for(let i = 0; i < halfLen; i++) {
+        if(string[i] != string[len - i - 1]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
