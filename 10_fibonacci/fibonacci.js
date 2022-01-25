08ec7f2fdc47bf76c70d6805afff24316ffcc340
Luka Colić <lcenzo@protonmail.ch>
2022-01-25 09:57:22 +0100

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(typeof(+num) !== "number") return "OOPS";
 
+    num = +num;
+
+    if(num < 0) return "OOPS";
+    if(num < 2) return num;
+    if(num == 2) return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
