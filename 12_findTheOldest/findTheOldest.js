cdd30776564a637b30891e6d05750117e603566b
Luka Colić <lcenzo@protonmail.ch>
2022-01-25 14:29:10 +0100

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    function older(p1, p2) {
+        const currYear = new Date().getFullYear();
+        let age1 = -p1.yearOfBirth + (p1.yearOfDeath === undefined ? currYear : p1.yearOfDeath);
+        let age2 = -p2.yearOfBirth + (p2.yearOfDeath === undefined ? currYear : p2.yearOfDeath);
+        return age1 >= age2;
+    }
+    
+    return people.reduce((p1, p2) => older(p1, p2) ? p1 : p2);
 };
 
 // Do not edit below this line
